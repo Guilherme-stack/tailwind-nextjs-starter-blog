8b89f2a9f71ec547f71b2970d214ac8628144e40
Guilherme de Lima <guilherme.limamota@hotmail.com>
2025-08-29 01:56:42 -0300

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -120,13 +120,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="text-black antialiased dark:bg-gray-950 dark:text-white">
-        {/*  <Script id="gtm" strategy="afterInteractive">
+        <Script id="gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-M99C9S4M');`}
-        </Script> */}
+        </Script>
 
         {/* GTM: noscript (fallback) — recomendado ficar logo após <body> */}
         <noscript>
